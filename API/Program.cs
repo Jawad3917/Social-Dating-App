d6cdacf1ce065488b4464d12e9b9a050ac20b7eb
Jawad3917 <xawadamir0@gmail.com>
2023-02-13 00:30:45 +0600

    Authentication Added

--- API/Program.cs
+++ API/Program.cs
@@ -1,19 +1,26 @@
 using API.Data;
+using API.Interfaces;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Linq.Expressions;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => {
-   opt.UseSqlite(builder.Configuration.GetConnectionString("defaultConnection"));
-});
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseAuthentication();
+app.UseAuthorization();
 
 
 app.MapControllers();
